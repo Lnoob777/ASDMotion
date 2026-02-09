390f920f5144f2ccc565550864b6741738753d95
Preethamkota <preethamkota28@gmail.com>
2026-02-09 10:22:06 +0530

    face encoder

--- src/cnn_encoders/face_encoder.py
+++ src/cnn_encoders/face_encoder.py
@@ -3,4 +3,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import torch.nn as nn
 import torch.optim as optim
+from torchvision.models import resnet18
 
+class FaceN(nn.Module):
+    def __init__(self,pretrained : bool=True):
+        super().__init__()
+
+        backbone = resnet18(pretrained=pretrained)
+        backbone.fc = nn.Identity()
